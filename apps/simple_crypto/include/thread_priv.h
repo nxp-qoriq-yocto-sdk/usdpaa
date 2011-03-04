6f9b1bfecd4975a3c0083450655553f4aa63c04b
Emil Medve <Emilian.Medve@Freescale.com>
2011-03-04 15:37:07 -0600

    simple_crypto: Don't use kernel header files
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>

--- apps/simple_crypto/include/thread_priv.h
+++ apps/simple_crypto/include/thread_priv.h
@@ -4,13 +4,13 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
+ *	 notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
+ *	 notice, this list of conditions and the following disclaimer in the
+ *	 documentation and/or other materials provided with the distribution.
  *     * Neither the name of Freescale Semiconductor nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ *	 names of its contributors may be used to endorse or promote products
+ *	 derived from this software without specific prior written permission.
  *
  *
  * ALTERNATIVELY, this software may be distributed under the terms of the
@@ -36,10 +36,11 @@
 #include <usdpaa/dma_mem.h>
 #include <usdpaa/fman.h>
 #include <usdpaa/fsl_usd.h>
+
+#include <internal/compat.h>
+
 #include <net/ethernet.h>
 #include <net/if_arp.h>
-#include <linux/ip.h>
-#include <internal/compat.h>
 
 #define MAX_THREADS 8
 
