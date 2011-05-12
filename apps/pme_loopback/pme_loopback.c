fb30b895782dcd3ad72ad7d7a81906d815cf813f
Emil Medve <Emilian.Medve@Freescale.com>
2011-05-12 13:58:19 -0500

    pme_loopback: Fix 64-bit portability warning(s)
    apps/pme_loopback/pme_loopback.c: In function ‘pme_loopback_cli_stop_scan’:
    apps/pme_loopback/pme_loopback.c:1158:2: warning: format ‘%llu’ expects argument of type ‘long long unsigned int’, but argument 2 has type ‘uint64_t’ [-Wformat]
    apps/pme_loopback/pme_loopback.c:1161:2: warning: format ‘%llu’ expects argument of type ‘long long unsigned int’, but argument 2 has type ‘uint64_t’ [-Wformat]
    apps/pme_loopback/pme_loopback.c:1162:2: warning: format ‘%llu’ expects argument of type ‘long long unsigned int’, but argument 2 has type ‘uint64_t’ [-Wformat]
    
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>

--- apps/pme_loopback/pme_loopback.c
+++ apps/pme_loopback/pme_loopback.c
@@ -4,13 +4,13 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
+ *	 notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
+ *	 notice, this list of conditions and the following disclaimer in the
+ *	 documentation and/or other materials provided with the distribution.
  *     * Neither the name of Freescale Semiconductor nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ *	 names of its contributors may be used to endorse or promote products
+ *	 derived from this software without specific prior written permission.
  *
  *
  * ALTERNATIVELY, this software may be distributed under the terms of the
@@ -28,6 +28,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 #include "pme_loopback.h"
 #include <unistd.h>
 #include <readline.h>
@@ -44,6 +45,7 @@
 #include <sys/resource.h>
 #include <time.h>
 #include <signal.h>
+#include <inttypes.h>
 
 #undef DEBUG
 #undef USE_MALLOC
@@ -172,7 +174,7 @@ static void add_worker_time(struct worker *worker)
 		worker->stats.total_usec = MICROSECONDS_PER_SECOND +
 			worker->stats.end_tv.tv_usec -
 			worker->stats.start_tv.tv_usec;
-		worker->stats.total_sec  =
+		worker->stats.total_sec	 =
 			worker->stats.end_tv.tv_sec -
 			worker->stats.start_tv.tv_sec - 1;
 	} else {
@@ -1304,11 +1306,11 @@ static int pme_loopback_cli_stop_scan(int argc, char *argv[])
 	bw = total_packet_size / num_workers;
 	bw *= 8*pps;
 	bw /= (1000*1000);
-	printf("Total units scanned: %llu\n", total_units);
+	printf("Total units scanned: %"PRIu64"\n", total_units);
 	printf("Total time: %f sec\n",
 		(total_time_sec+total_time_usec*1e-6)/num_workers);
-	printf("Scan Units per second: %llu\n", pps);
-	printf("Bandwidth: %llu Mbps\n", bw);
+	printf("Scan Units per second: %"PRIu64"\n", pps);
+	printf("Bandwidth: %"PRIu64" Mbps\n", bw);
 	return 0;
 }
 
