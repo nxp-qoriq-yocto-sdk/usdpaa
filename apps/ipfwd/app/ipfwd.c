nxp-qoriq-yocto-sdk/usdpaa

commit fbe362bb8f062003751c011a919c432bc6e0edea

ipfwd: Make the TX FQ a function of the RX FQ
This ensures that all the frames received on a certain FQ will always be transmitted on the same FQ

Signed-off-by: Mahima Verma <Mahima.Verma@freescale.com>
Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>

apps/ipfwd/app/ipfwd.c
@@ -639,7 +639,11 @@ static int ppam_if_init(struct ppam_if *p,
 	p->mtu = ETHERMTU;
 	p->header_len = ETHER_HDR_LEN;
 	p->mask = IN_CLASSC_NET;
-	p->next_fqid = 0;
+
+	p->num_tx_fqids = num_tx_fqs;
+	p->tx_fqids = malloc(p->num_tx_fqids * sizeof(*p->tx_fqids));
+	if (unlikely(p->tx_fqids == 0))
+		return -ENOMEM;
 
 	eth_setup(p);
 
@@ -647,9 +651,11 @@ static int ppam_if_init(struct ppam_if *p,
 }
 static void ppam_if_finish(struct ppam_if *p)
 {
+	free(p->tx_fqids);
 }
 static void ppam_if_tx_fqid(struct ppam_if *p, unsigned idx, uint32_t fqid)
 {
+	p->tx_fqids[idx] = fqid;
 }
 static int ppam_rx_error_init(struct ppam_rx_error *p, struct ppam_if *_if,
 			      struct qm_fqd_stashing *stash_opts)
@@ -724,6 +730,9 @@ static int ppam_rx_hash_init(struct ppam_rx_hash *p, struct ppam_if *_if,
 	p->hooks = &stack.hooks;
 	p->protos = &stack.protos;
 	p->rc = &stack.rc;
+
+	p->tx_fqid = _if->tx_fqids[idx % _if->num_tx_fqids];
+
 	/* Override defaults, enable 1 CL of annotation stashing */
 	stash_opts->annotation_cl = 1;
 

apps/ipfwd/include/ppam_if.h
@@ -40,11 +40,12 @@ struct ppam_if
 	in_addr_t addr, mask;
 	int ifnum;
 
+	size_t num_tx_fqids;
+	uint32_t *tx_fqids;
+
 	void (*set_header)(const struct ppac_if *i, void *payload, const void *src, const void *dst);
 	void (*cache_header)(struct ll_cache_t *llc, const void *hdr);
 	void (*output_header)(void *hdr, const struct ll_cache_t *llc);
-
-	size_t next_fqid;
 
 	struct node_t local_nodes[23];
 };
@@ -74,6 +75,8 @@ struct ppam_rx_hash
 	struct ip_hooks_t *hooks;
 	struct ip_protos_t *protos;
 	struct rc_t *rc;
+
+	uint32_t tx_fqid;
 };
 
 #endif	/* __PPAM_IF_H */

apps/ipfwd/lib/ip/ip_output.c
@@ -98,14 +98,13 @@ enum IP_STATUS ip_output(const struct ppam_rx_hash *ctxt,
 /*
  * Find the correct neighbor for this frame, using ARP tables
  */
-enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt __always_unused,
+enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt,
 				struct annotations_t *notes,
 				struct iphdr *ip_hdr,
 				enum state source)
 {
 	struct ll_cache_t *ll_cache;
 	struct neigh_t *neighbor;
-	struct ppac_if *i;
 	enum IP_STATUS retval;
 	struct ether_header *ll_hdr;
 #ifdef NOT_USDPAA
@@ -118,7 +117,6 @@ enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt __always_unused,
 	retval = IP_STATUS_ACCEPT;
 
 	neighbor = notes->dest->neighbor;
-	i = neighbor->dev;
 	ll_cache = neighbor->ll_cache;
 
 	if (unlikely(ll_cache == NULL)) {
@@ -160,15 +158,14 @@ enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt __always_unused,
 		neighbor->retransmit_count = 0;
 	} else {
 		ll_hdr = (void *)ip_hdr - ll_cache->ll_hdr_len;
-		i->module_if.output_header(ll_hdr, ll_cache);
+		neighbor->dev->module_if.output_header(ll_hdr, ll_cache);
 #ifdef IPSECFWD_HYBRID_GENERATOR
 		eth_header_swap(ll_hdr);
 		temp = ip_hdr->src_addr;
 		ip_hdr->src_addr = ip_hdr->dst_addr;
 		ip_hdr->dst_addr = temp;
 #endif
-		ppac_send_frame(qman_fq_fqid(i->tx_fqs + i->module_if.next_fqid), notes->fd);
-		i->module_if.next_fqid = (i->module_if.next_fqid + 1) % i->num_tx_fqs;
+		ppac_send_frame(ctxt->tx_fqid, notes->fd);
 	}
 
 	return retval;

commit 357c7d0842a452f747b2d18fe1869b6c4c3c1572

ipfwd: Forward/enqueue packets on the destination interface TX FQID(s)
This fixes a bug introduced in fbe362b, where frames were forwarded/enquued on the source interface
TX FQID(s)

Signed-off-by: Mahima Verma <Mahima.Verma@freescale.com>
Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>

apps/ipfwd/app/ipfwd.c
@@ -732,8 +732,6 @@ static int ppam_rx_hash_init(struct ppam_rx_hash *p, struct ppam_if *_if,
 	p->protos = &stack.protos;
 	p->rc = &stack.rc;
 
-	p->tx_fqid = _if->tx_fqids[idx % _if->num_tx_fqids];
-
 	/* Override defaults, enable 1 CL of annotation stashing */
 	stash_opts->annotation_cl = (sizeof(struct annotations_t) + L1_CACHE_BYTES - 1) /
 		L1_CACHE_BYTES;
@@ -762,6 +760,7 @@ static inline void ppam_rx_hash_cb(struct ppam_rx_hash *p,
 		return;
 	}
 	notes->fd = &dqrr->fd;
+	notes->fqid = dqrr->fqid;
 
 	ip_handler(p, notes, data);
 }

apps/ipfwd/include/ppam_if.h
@@ -75,8 +75,6 @@ struct ppam_rx_hash
 	struct ip_hooks_t *hooks;
 	struct ip_protos_t *protos;
 	struct rc_t *rc;
-
-	uint32_t tx_fqid;
 };
 
 #endif	/* __PPAM_IF_H */

apps/ipfwd/lib/ip/ip_output.c
@@ -105,6 +105,7 @@ enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt,
 {
 	struct ll_cache_t *ll_cache;
 	struct neigh_t *neighbor;
+	const struct ppam_if *p;
 	enum IP_STATUS retval;
 	struct ether_header *ll_hdr;
 #ifdef NOT_USDPAA
@@ -158,14 +159,15 @@ enum IP_STATUS ip_output_finish(const struct ppam_rx_hash *ctxt,
 		neighbor->retransmit_count = 0;
 	} else {
 		ll_hdr = (void *)ip_hdr - ll_cache->ll_hdr_len;
-		neighbor->dev->module_if.output_header(ll_hdr, ll_cache);
+		p = &neighbor->dev->module_if;
+		p->output_header(ll_hdr, ll_cache);
 #ifdef IPSECFWD_HYBRID_GENERATOR
 		eth_header_swap(ll_hdr);
 		temp = ip_hdr->src_addr;
 		ip_hdr->src_addr = ip_hdr->dst_addr;
 		ip_hdr->dst_addr = temp;
 #endif
-		ppac_send_frame(ctxt->tx_fqid, notes->fd);
+		ppac_send_frame(p->tx_fqids[notes->fqid % p->num_tx_fqids], notes->fd);
 	}
 
 	return retval;

apps/ipfwd/lib/net/annotations.h
@@ -25,8 +25,8 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef LIB_COMMON_ANNOTATIONS_H
-#define LIB_COMMON_ANNOTATIONS_H
+#ifndef __ANNOTATIONS_H
+#define __ANNOTATIONS_H
 
 #include <usdpaa/compat.h>
 #include <usdpaa/fsl_qman.h>
@@ -43,10 +43,11 @@ struct annotations_t {
 	/**< Pointer to frame descriptor*/
 	struct rt_dest_t *dest;
 	/**< Pointer to the info related to Next Hop*/
-	uint8_t reserved1[8];
+	uint32_t fqid;
+	uint8_t reserved1[4];
 	struct output_parse_result_t parse;	/**< Pointer to Parsed result*/
 	uint64_t timestamp;			/**< TimeStamp */
 	uint64_t hash_result;			/**< Hash Result */
 } __PACKED;
 
-#endif	/* LIB_COMMON_ANNOTATIONS_H */
+#endif	/* __ANNOTATIONS_H */