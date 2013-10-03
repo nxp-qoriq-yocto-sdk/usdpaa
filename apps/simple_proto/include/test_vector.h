nxp-qoriq-yocto-sdk/usdpaa

commit 2e328155979f749a0a91f9b0f949c0aa59686549

simple_proto: add WiFi tests
Added in simple_proto application support for WiFi tests.
Note: WiFi tests are running with only one iteration in CIPHER mode.
This limitation is due to the PN incrementation in operation
command together with the fix included for Errata A005487:
PN written in reverse order in CCMP header.

Change-Id: Idcce5950f44743a8185837e1e82a984448058f49
Signed-off-by: Carmen Iorga <Carmen.Iorga@freescale.com>
Signed-off-by: Oana Barbu <oana.barbu@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/4957
Reviewed-by: Vakul Garg <vakul@freescale.com>
Tested-by: Vakul Garg <vakul@freescale.com>

apps/simple_proto/app/simple_proto.c
@@ -344,6 +344,18 @@ int test_dec_match_cb_pdcp(int fd_ind, uint8_t *dec_buf,
 	return 0;
 }
 
+int test_enc_match_cb_wifi(int fd_ind, uint8_t *enc_buf,
+			    struct test_param *crypto_info)
+{
+	if (!fd_ind &&
+	    test_vector_match((uint32_t *)enc_buf,
+			      (uint32_t *)ref_test_vector.ciphertext,
+			      crypto_info->rt.output_buf_size * BITS_PER_BYTE))
+		return -1;
+
+	return 0;
+}
+
 void init_rtv_pdcp(struct test_param *crypto_info)
 {
 	struct pdcp_params *pdcp_params =
@@ -492,6 +504,32 @@ void init_rtv_srtp(struct test_param *crypto_info)
 	}
 }
 
+void init_rtv_wifi_ccmp(struct test_param *crypto_info)
+{
+	strcpy(protocol, "WiFi");
+	ref_test_vector.key =
+		(uintptr_t)wifi_reference_key[crypto_info->test_set - 1];
+
+	/* set the WiFi pdb params for test */
+	ref_test_vector.pdb.wifi.mac_hdr_len =
+	    wifi_reference_mac_hdr_len[crypto_info->test_set - 1];
+	ref_test_vector.pdb.wifi.pn =
+	    wifi_reference_pn[crypto_info->test_set - 1];
+	ref_test_vector.pdb.wifi.priority =
+	    wifi_reference_pri[crypto_info->test_set - 1];
+	ref_test_vector.pdb.wifi.key_id =
+	    wifi_reference_key_id[crypto_info->test_set - 1];
+
+	if (CIPHER == crypto_info->mode) {
+		ref_test_vector.length =
+		    wifi_reference_length[crypto_info->test_set - 1];
+		ref_test_vector.plaintext =
+		    wifi_reference_plaintext[crypto_info->test_set - 1];
+		ref_test_vector.ciphertext =
+		    wifi_reference_ciphertext[crypto_info->test_set - 1];
+	}
+}
+
 /**
  * @brief	Set PN constant in MACsec shared descriptor
  * @details	Inside this routine, context is erased, PN is read from
@@ -576,7 +614,8 @@ void (*init_ref_test_vector[]) (struct test_param *crypto_info) = {
 	init_rtv_macsec,
 	init_rtv_wimax_aes_ccm_128,
 	init_rtv_pdcp,
-	init_rtv_srtp
+	init_rtv_srtp,
+	init_rtv_wifi_ccmp
 };
 
 /**
@@ -745,6 +784,12 @@ static int set_buf_size(struct test_param *crypto_info)
 		crypto_info->rt.output_buf_size =
 			crypto_info->buf_size + ref_test_vector.pdb.srtp.n_tag;
 		break;
+
+	case WIFI:
+			crypto_info->rt.output_buf_size =
+			    crypto_info->buf_size + WIFI_CCM_HDR_SIZE +
+			    WIFI_ICV_SIZE;
+			break;
 	default:
 		fprintf(stderr, "error: %s: protocol not supported\n",
 			__func__);
@@ -953,6 +998,39 @@ static void *setup_init_descriptor(bool mode, struct test_param *crypto_info)
 					ref_test_vector.pdb.srtp.seqnum,
 					ref_test_vector.pdb.srtp.cipher_salt);
 		break;
+
+	case WIFI:
+		cipher_info.key = ref_test_vector.key;
+		cipher_info.keylen = WIFI_KEY_SIZE;
+		cipher_info.key_enc_flags = 0;
+
+		if (ENCRYPT == mode)
+			cnstr_shdsc_wifi_encap(shared_desc,
+					&shared_desc_len,
+/*
+ * This is currently hardcoded. The application doesn't allow for
+ * proper retrieval of PS.
+*/
+					0,
+					ref_test_vector.pdb.wifi.mac_hdr_len,
+					ref_test_vector.pdb.wifi.pn,
+					ref_test_vector.pdb.wifi.priority,
+					ref_test_vector.pdb.wifi.key_id,
+					&cipher_info);
+		else
+			cnstr_shdsc_wifi_decap(shared_desc,
+					&shared_desc_len,
+/*
+ * This is currently hardcoded. The application doesn't allow for
+ * proper retrieval of PS.
+*/
+					0,
+					ref_test_vector.pdb.wifi.mac_hdr_len,
+					ref_test_vector.pdb.wifi.pn,
+					ref_test_vector.pdb.wifi.priority,
+					&cipher_info);
+		break;
+
 	default:
 		fprintf(stderr, "error: %s: protocol not supported\n",
 			__func__);
@@ -1089,6 +1167,7 @@ struct argp_option options[] = {
 	"\n 2 for WiMAX"
 	"\n 3 for PDCP"
 	"\n 4 for SRTP"
+	"\n 5 for WiFi"
 	"\n"},
 	{"itrnum", 'l', "ITERATIONS", 0,
 	"Number of iterations to repeat"
@@ -1367,6 +1446,11 @@ static int validate_test_set(struct test_param *crypto_info)
 			return 0;
 		goto err;
 
+	case WIFI:
+		if ((crypto_info->test_set > 0) && (crypto_info->test_set < 3))
+			return 0;
+		goto err;
+
 	default:
 		fprintf(stderr,
 			"error: Invalid Parameters: Invalid SEC protocol\n");
@@ -1642,6 +1726,33 @@ static int validate_srtp_opts(uint32_t g_proto_params,
 }
 
 /**
+ * @brief	Check SEC parameters provided by user for WiFi are valid
+ *		or not.
+ * @param[in]	g_proto_params - Bit mask of the optional parameters provided
+ *		by user
+ * @param[in]	crypto_info - test parameters
+ * @return	0 on success, otherwise -EINVAL value
+ */
+static int validate_wifi_opts(uint32_t g_proto_params,
+				struct test_param *crypto_info)
+{
+	/*
+	 * For WiFi in CIPHER mode only the first frame
+	 * from the first iteration can be verified if it is matching
+	 * with the corresponding test vector, due to
+	 * the PN incrementation by SEC for each frame processed.
+	 */
+	if (CIPHER == crypto_info->mode && crypto_info->itr_num != 1) {
+		crypto_info->itr_num = 1;
+		printf("WARNING: Running WiFi in CIPHER mode with only one iteration\n");
+	}
+
+	crypto_info->test_enc_match_cb = test_enc_match_cb_wifi;
+
+	return 0;
+}
+
+/**
  * @brief	Check SEC parameters provided by user whether valid or not
  * @param[in]	g_cmd_params - Bit mask of all parameters provided by user
  * @param[in]	g_proto_params - Bit mask of protocol specific parameters, as
@@ -1718,6 +1829,7 @@ static int validate_params(uint32_t g_cmd_params, uint32_t g_proto_params,
 	case WIMAX:
 	case PDCP:
 	case SRTP:
+	case WIFI:
 		return validate_proto_opts[crypto_info->proto]
 					(g_proto_params, crypto_info);
 	default:

apps/simple_proto/include/test_vector.h
@@ -121,6 +121,12 @@ struct ref_vector_s {
 			uint8_t options; /* optional */
 			uint32_t mki;    /* optional */
 		} srtp;
+		struct {
+			uint16_t mac_hdr_len;
+			uint64_t pn;
+			uint8_t priority;
+			uint8_t key_id;
+		} wifi;
 	} __packed pdb;
 	union {
 		struct {
@@ -2443,4 +2449,78 @@ static uint8_t srtp_reference_n_tag[] = { 0x0F };
 static uint16_t srtp_reference_seq_num[] = { 0x4BEE };
 static uint32_t srtp_reference_roc[] = { 0x6AD47CD7 };
 
+/* WIFI Test Vectors - Taken from IEEE 802.11 - 03/131r4 document */
+#define WIFI_KEY_SIZE      16
+#define WIFI_CCM_HDR_SIZE  8
+#define WIFI_ICV_SIZE      8
+
+static uint8_t wifi_reference_key[][WIFI_KEY_SIZE] = {
+	/* Test set 1 */
+	{0xc9, 0x7c, 0x1f, 0x67, 0xce, 0x37, 0x11, 0x85, 0x51, 0x4a, 0x8a, 0x19,
+	 0xf2, 0xbd, 0xd5, 0x2f},
+	/* Test set 1 */
+	{0x8f, 0x7a, 0x05, 0x3f, 0xa5, 0x77, 0xa5, 0x59, 0x75, 0x29, 0x27, 0x20,
+	 0x97, 0xa6, 0x03, 0xd5}
+};
+
+static uint16_t wifi_reference_mac_hdr_len[] = {
+	/* Test set 1 */
+	0x0018,
+	/* Test set 2 */
+	0x0018
+};
+
+static uint64_t wifi_reference_pn[] = {
+	/* Test set 1 */
+	0xB5039776E70C,
+	/* Test set 2 */
+	0x31F3CBBA97EA,
+};
+
+static uint8_t wifi_reference_pri[] = {
+	/* Test set 1 */
+	0x00,
+	/* Test set 2 */
+	0x00
+};
+
+static uint8_t wifi_reference_key_id[] = {
+	/* Test set 1 */
+	0x20,
+	/* Test set 2 */
+	0xa0
+};
+
+/* length in bits */
+static uint32_t wifi_reference_length[] = {352, 352};
+
+static uint8_t wifi_reference_plaintext[][MAX_TEST_PLAIN_PACKET_SIZE] = {
+	/* Test set 1 */
+	{0x08, 0x48, 0xc3, 0x2c, 0x0f, 0xd2, 0xe1, 0x28, 0xa5, 0x7c, 0x50, 0x30,
+	 0xf1, 0x84, 0x44, 0x08, 0xab, 0xae, 0xa5, 0xb8, 0xfc, 0xba, 0x80, 0x33,
+	 0xf8, 0xba, 0x1a, 0x55, 0xd0, 0x2f, 0x85, 0xae, 0x96, 0x7b, 0xb6, 0x2f,
+	 0xb6, 0xcd, 0xa8, 0xeb, 0x7e, 0x78, 0xa0, 0x50},
+	 /* Test set 2 */
+	{0x38, 0xc0, 0x6a, 0x51, 0xea, 0x10, 0x0c, 0x84, 0x68, 0x50, 0xee, 0xc1,
+	 0x76, 0x2c, 0x88, 0xde, 0xaf, 0x2e, 0xe9, 0xf4, 0x6a, 0x07, 0xe0, 0xcc,
+	 0x83, 0xa0, 0x63, 0x4b, 0x5e, 0xd7, 0x62, 0x7e, 0xb9, 0xdf, 0x22, 0x5e,
+	 0x05, 0x74, 0x03, 0x42, 0xde, 0x19, 0x41, 0x17}
+};
+
+static uint8_t wifi_reference_ciphertext[][MAX_TEST_ENCRYPT_PACKET_SIZE] = {
+	/* Test set 1 */
+	{0x08, 0x48, 0xc3, 0x2c, 0x0f, 0xd2, 0xe1, 0x28, 0xa5, 0x7c, 0x50, 0x30,
+	 0xf1, 0x84, 0x44, 0x08, 0xab, 0xae, 0xa5, 0xb8, 0xfc, 0xba, 0x80, 0x33,
+	 0x0c, 0xe7, 0x00, 0x20, 0x76, 0x97, 0x03, 0xb5, 0xf3, 0xd0, 0xa2, 0xfe,
+	 0x9a, 0x3d, 0xbf, 0x23, 0x42, 0xa6, 0x43, 0xe4, 0x32, 0x46, 0xe8, 0x0c,
+	 0x3c, 0x04, 0xd0, 0x19, 0x78, 0x45, 0xce, 0x0b, 0x16, 0xf9, 0x76, 0x23
+	},
+	/* Test set 2 */
+	{0x38, 0xc0, 0x6a, 0x51, 0xea, 0x10, 0x0c, 0x84, 0x68, 0x50, 0xee, 0xc1,
+	 0x76, 0x2c, 0x88, 0xde, 0xaf, 0x2e, 0xe9, 0xf4, 0x6a, 0x07, 0xe0, 0xcc,
+	 0xea, 0x97, 0x00, 0xa0, 0xba, 0xcb, 0xf3, 0x31, 0x81, 0x4b, 0x69, 0x65,
+	 0xd0, 0x5b, 0xf2, 0xb2, 0xed, 0x38, 0xd4, 0xbe, 0xb0, 0x69, 0xfe, 0x82,
+	 0x71, 0x4a, 0x61, 0x0b, 0x54, 0x2f, 0xbf, 0x8d, 0xa0, 0x6a, 0xa4, 0xae
+	}
+};
 #endif /* __SIMPLE_PROTO_TEST_VECTOR_H */

apps/simple_proto/include/simple_proto.h
@@ -125,7 +125,8 @@ enum sec_proto {
 	MACSEC = 1,
 	WIMAX,
 	PDCP,
-	SRTP
+	SRTP,
+	WIFI
 };
 
 /**
@@ -215,6 +216,7 @@ void init_rtv_pdcp(struct test_param *crypto_info);
 void init_rtv_pdcp_c_plane(struct test_param *crypto_info);
 void init_rtv_pdcp_u_plane(struct test_param *crypto_info);
 void init_rtv_pdcp_short_mac(struct test_param *crypto_info);
+void init_rtv_wifi_ccmp(struct test_param *crypto_info);
 
 /* test cleanup routines */
 void test_cleanup_wimax(struct test_param *crypto_info);
@@ -240,19 +242,24 @@ static int validate_pdcp_opts(uint32_t g_proto_params,
 				struct test_param *crypto_info);
 static int validate_srtp_opts(uint32_t g_proto_params,
 			      struct test_param *crypto_info);
+static int validate_wifi_opts(uint32_t g_proto_params,
+			      struct test_param *crypto_info);
 int test_enc_match(void *params, struct qm_fd fd[]);
 int test_dec_match(void *params, struct qm_fd fd[]);
 int test_enc_match_cb_wimax(int fd_ind, uint8_t *enc_buf,
 			    struct test_param *crypto_info);
 int test_dec_match_cb_wimax(int fd_ind, uint8_t *enc_buf,
 			    struct test_param *crypto_info);
+int test_enc_match_cb_wifi(int fd_ind, uint8_t *enc_buf,
+			   struct test_param *crypto_info);
 error_t parse_opt(int opt, char *arg, struct argp_state *state);
 int (*validate_proto_opts[])(uint32_t, struct test_param*) = {
 		NULL,
 		validate_macsec_opts,
 		validate_wimax_opts,
 		validate_pdcp_opts,
-		validate_srtp_opts
+		validate_srtp_opts,
+		validate_wifi_opts
 };
 
 /* helper routines */

commit 115a9e21460fd688b56b16ae80551acd89ea52f4

simple_proto: add WiFi defines
Add proper defines for WiFi. This commit solves the problems introduced
by commit 2e328155979f749a0a91f9b0f949c0aa59686549.

Change-Id: Ib26698b3e19d40846ac3d6729f11a036f3945a67
Signed-off-by: Oana Barbu <oana.barbu@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/5170
Reviewed-by: Geanta Neag Horia Ioan-B05471 <horia.geanta@freescale.com>
Reviewed-by: Vakul Garg <vakul@freescale.com>
Tested-by: Vakul Garg <vakul@freescale.com>

apps/simple_proto/app/simple_proto.c
@@ -716,6 +716,11 @@ static int get_buf_size(struct test_param *crypto_info)
 	case SRTP:
 		total_size += crypto_info->buf_size + SRTP_MAX_ICV_SIZE;
 		break;
+
+	case WIFI:
+		total_size += crypto_info->buf_size + WIFI_CCM_SIZE +
+				WIFI_ICV_SIZE + WIFI_FCS_SIZE;
+		break;
 	}
 
 	return total_size;

apps/simple_proto/include/test_vector.h
@@ -64,6 +64,16 @@
 
 #define PDCP_HFN_DUMMY_VAL	0x1234
 
+#define WIFI_CCM_SIZE		8
+#define WIFI_ICV_SIZE		8
+#define WIFI_FCS_SIZE		4
+
+#define WIFI_MAX_TEST_PLAIN_PACKET_SIZE	44
+#define WIFI_MAX_TEST_ENCRYPT_PACKET_SIZE	\
+					(WIFI_MAX_TEST_PLAIN_PACKET_SIZE +\
+					 WIFI_CCM_SIZE +\
+					 WIFI_ICV_SIZE)
+
 /** test vector structure which holds essential initialization vector, key etc;
  *  and cipher/plain text to compare with encapsulated/decapsulated data
  *  generated by SEC
@@ -2494,7 +2504,7 @@ static uint8_t wifi_reference_key_id[] = {
 /* length in bits */
 static uint32_t wifi_reference_length[] = {352, 352};
 
-static uint8_t wifi_reference_plaintext[][MAX_TEST_PLAIN_PACKET_SIZE] = {
+static uint8_t wifi_reference_plaintext[][WIFI_MAX_TEST_PLAIN_PACKET_SIZE] = {
 	/* Test set 1 */
 	{0x08, 0x48, 0xc3, 0x2c, 0x0f, 0xd2, 0xe1, 0x28, 0xa5, 0x7c, 0x50, 0x30,
 	 0xf1, 0x84, 0x44, 0x08, 0xab, 0xae, 0xa5, 0xb8, 0xfc, 0xba, 0x80, 0x33,
@@ -2507,7 +2517,7 @@ static uint8_t wifi_reference_plaintext[][MAX_TEST_PLAIN_PACKET_SIZE] = {
 	 0x05, 0x74, 0x03, 0x42, 0xde, 0x19, 0x41, 0x17}
 };
 
-static uint8_t wifi_reference_ciphertext[][MAX_TEST_ENCRYPT_PACKET_SIZE] = {
+static uint8_t wifi_reference_ciphertext[][WIFI_MAX_TEST_ENCRYPT_PACKET_SIZE] = {
 	/* Test set 1 */
 	{0x08, 0x48, 0xc3, 0x2c, 0x0f, 0xd2, 0xe1, 0x28, 0xa5, 0x7c, 0x50, 0x30,
 	 0xf1, 0x84, 0x44, 0x08, 0xab, 0xae, 0xa5, 0xb8, 0xfc, 0xba, 0x80, 0x33,